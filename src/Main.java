4fb9ea185a1f98b559b331d1a6f0a2dbc0992e7e
mslancer <wolfnw@yandex.ru>
2021-07-19 20:50:46 +0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int bank = 1000;
-        int put = 1100;
+        int put = 1000;
         int bonus;
-        if (put >= 1000) {
+        if (put > 1000) {
             bonus = put / 100 + put + bank;
         } else {
             bonus = put + bank;
         }
 
-
+        System.out.println("Сумма на карте");
         System.out.println(bonus);
     }
 }
